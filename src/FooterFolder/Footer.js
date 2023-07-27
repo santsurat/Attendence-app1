30e463b5eab861f31961cbd854a9a637592211ad
@SantSurat <santsurat123@gmail.com>
2023-07-27 12:03:53 +0530

    Update Footer.js

--- src/FooterFolder/Footer.js
+++ src/FooterFolder/Footer.js
@@ -6,13 +6,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-section contact-info">
-          <h2>Contact Us</h2>
-          <p>Email: ups10@gmail.com.com</p>
-          <p>Phone: 7295018814</p>
-          <p>Address: chhota govindpur near vivek vidyalaya, jamshedpur, India</p>
-        </div>
+      
 
         <div className="footer-section navigation-links">
           <h2>Quick Links</h2>
@@ -31,19 +25,11 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="footer-section social-media">
-          <h2>Follow Us</h2>
-          <ul>
-          <li><a href="https://www.linkdien.com/example">LinkedIn</a></li>
-          <li><a href="https://www.twitter.com/example">Twitter</a></li>
-          <li><a href="https://www.instagram.com/example">Instagram</a></li>
-          </ul>
-        </div>
-      </div>
+        
 
       <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} sant surat. All rights reserved.</p>
-        <p><a href="/privacy">Privacy Policy</a> | <a href="/terms">Terms of Use</a></p>
+        <p>&copy; {new Date().getFullYear()} Sant Surat,Inc</p>
+        <p><a href="/privacy" style={{color:'white'}}>Privacy Policy</a> | <a href="/terms" style={{color:'white'}}>Terms of Use</a></p>
       </div>
     </footer>
   );
